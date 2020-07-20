3eb5e8a42f61080acc856a9ca0f72f7d75d674f2
maxime.yon <maxime.yon@gmail.com>
2020-07-20 11:58:38 +0200

    update extract diff gadget

--- gadgets/mri_core/ExtractDiffGadget.cpp
+++ gadgets/mri_core/ExtractDiffGadget.cpp
@@ -41,21 +41,31 @@ int ExtractDiffGadget::process(GadgetContainerMessage<ISMRMRD::ImageHeader> *m1,
 
    // std::cout << m1->getObjectPtr()->image_series_index << std::endl;
 
-    if (m1->getObjectPtr()->image_series_index==5000)
+    if (m1->getObjectPtr()->image_series_index==6000)
     {
         m3->getObjectPtr()->set(GADGETRON_IMAGECOMMENT, "ADC");
         m3->getObjectPtr()->set(GADGETRON_SEQUENCEDESCRIPTION, "ADC");    
         m3->getObjectPtr()->set(GADGETRON_IMAGEPROCESSINGHISTORY, "GT");
         //cm3->getObjectPtr()->set(GADGETRON_IMAGE_COLORMAP,  "HotMetal.pal");
-        m3->getObjectPtr()->set(GADGETRON_IMAGE_COLORMAP,  "Perfusion.pal");
+        //m3->getObjectPtr()->set(GADGETRON_IMAGE_COLORMAP,  "Perfusion.pal");
         m3->getObjectPtr()->set(GADGETRON_IMAGE_WINDOWCENTER, (long)(1500));
         m3->getObjectPtr()->set(GADGETRON_IMAGE_WINDOWWIDTH, (long)(3000));
+	std::cout << "ADC map section" << std::endl;
+    }
+
+    else if (m1->getObjectPtr()->image_series_index==5000)
+    {
+	std::cout << "Trace image section" << std::endl;
+        m3->getObjectPtr()->set(GADGETRON_IMAGECOMMENT, "TRACEWEIGHTED");
+        m3->getObjectPtr()->set(GADGETRON_SEQUENCEDESCRIPTION, "TRACEWEIGHTED");
+        m3->getObjectPtr()->set(GADGETRON_IMAGEPROCESSINGHISTORY, "GT");
     }
     else
     {       
         m3->getObjectPtr()->set(GADGETRON_IMAGECOMMENT, "MAGNITUDE");
         m3->getObjectPtr()->set(GADGETRON_SEQUENCEDESCRIPTION, "MAGNITUDE");
         m3->getObjectPtr()->set(GADGETRON_IMAGEPROCESSINGHISTORY, "GT");
+	std::cout << "DWI image section" << std::endl;
     }
 
     //m1->getObjectPtr()->image_series_index += series_offset.at(image_type);
